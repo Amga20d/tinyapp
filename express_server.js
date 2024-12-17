496c1fd909301252cac7875a45a5e06afa040c5d
Amgad Abdullah <amga200d@gmail.com>
2024-12-16 23:48:05 -0700

    Add POST route to express_server to recieve form submission

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -11,6 +12,11 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok"); 
 });
 
 app.get("/urls.json", (req, res) => {
@@ -38,3 +44,5 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {};
