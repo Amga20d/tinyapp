3cd7b4a64094bc0c37e64b7c6728d84e532d32f7
Amgad Abdullah <amga200d@gmail.com>
2024-12-17 01:52:48 -0700

    Added a post request to /login to set a cookie

--- express_server.js
+++ express_server.js
@@ -35,6 +35,12 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
