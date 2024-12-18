fabc40881ea1cb920a66e8b79cd9af7aef66fa34
Amgad Abdullah <amga200d@gmail.com>
2024-12-18 01:08:26 -0700

    more permission features

--- express_server.js
+++ express_server.js
@@ -147,12 +147,12 @@ app.get("/urls/:id", (req, res) => {
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
-  if (longURL) {
-    res.redirect(longURL);
-  } else {
-    res.status(404).send("URL not found");
+  if (!longURL) {
+    return res.status(404).send("<html><body><h3>URL not found</h3></body></html>");
   }
+  res.redirect(longURL);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
