a9ddad086f75a41f00207a18fbbed4eef7b50d14
Amgad Abdullah <amga200d@gmail.com>
2024-12-18 00:18:20 -0700

    Updated the logout handler to redirect to the login page

--- express_server.js
+++ express_server.js
@@ -65,7 +65,7 @@ app.post("/login", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie('user_id');
-  res.redirect('/urls');
+  res.redirect('/login');
 });
 
 app.get("/register", (req, res) => {
